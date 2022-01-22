dcdf878cc2fb079009ba997587198c01316fd2b5
Hunter O'Toole <90637885+Topzombie22@users.noreply.github.com>
2022-01-22 14:07:51 -0400

    Controller working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private SpriteRenderer pSprite;
+    [SerializeField]
     private Rigidbody2D rb;
     [SerializeField]
     private float speed;
@@ -15,11 +19,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private bool canJump;
     [SerializeField]
-    private bool isGrounded;
+    private bool canRaycast;
     [SerializeField]
     private bool canMove;
     [SerializeField]
-    private Vector2 input;
+    private float input;
+    [SerializeField]
+    private Vector2 movement;
+    [SerializeField]
+    private bool canDash;
+    [SerializeField]
+    private float dashDist;
 
 
     void Start()
@@ -33,10 +43,51 @@ public class PlayerController : MonoBehaviour
         Move();
     }
 
+    private void FixedUpdate()
+    {
+
+    }
+
     private void Move()
     {
-        input = new Vector2(Input.GetAxis("Horizontal"), 0f);
-        Vector2 movement = (input) * speed;
-        rb.velocity = movement;
+        input = (Input.GetAxis("Horizontal"));
+        transform.position += new Vector3(input, 0, 0) * Time.deltaTime * speed;
+        
+        if (canRaycast == true)
+        {
+            if (Physics2D.Raycast(player.position - new Vector3(0, pSprite.bounds.extents.y + 0.01f, 0), Vector2.down, rayCastDist))
+            {
+                canJump = true;
+            }
+        }
+
+        if (canJump == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = (new Vector2(rb.velocity.x, 0));
+            rb.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+            canJump = false;
+            canRaycast = false;
+            StartCoroutine(RaycastTimer());
+        }
+
+        //if (canDash == true && Input.GetKeyDown(KeyCode.LeftShift))
+        //{
+        //    if (rb.velocity.x > 0.01)
+        //    {
+        //      //  Vector3.Lerp(new Vector3(player.transform.position.x))
+        //    }
+
+        //    if (rb.velocity.x < -0.01)
+        //    {
+
+        //    }
+        //}
     }
+
+    IEnumerator RaycastTimer()
+    {
+        yield return new WaitForSeconds(0.1f);
+        canRaycast = true;
+    }
+
 }
